c57e1886c555e69a409470f3052f3f011033c5c0
Lyle Hubbard <lhubbard@pdx.edu>
2022-01-20 15:29:54 -0800

    moved parser contents to main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import argparse
 
 # Benchmarking
 #import . as bench
@@ -11,21 +12,35 @@ import mersenne.lucas as ll
 
 
 def main():
+    parser = argparse.ArgumentParser()
+
+    #in order to add more arguments to the parser, attempt a similar declaration to below. Anthing without a dash is becomes ordinal and required
+    parser.add_argument("-p","--prime",type = int, help="seed for the mersenne prime being tested", default = None)
+    parser.add_argument("--ll", action = "store_true")
+    parser.add_argument("--fft",type = str, default = None)
+    parser.add_argument("--bench",action = "store_true", help = "perform testing etc")
+    args = vars(parser.parse_args())
+    if not args["prime"]:
+	      raise ValueError("runtime requires a prime number for testing!")
+	      exit()
+
+    #args is a dictionary in python types, in a per flag key-value mapping, which can be accessed via, 
+    #eg, flags["prime"], which will return the integer passed in.
+    #If you want specific behavior for the options, eg prime is none, exit()""
     # Command line arguments
-    args = sys.argv[1:]
     
     # Argument option to load from file?
     
     # Determine which function is wanted,
     # Run relevant function
     
-    if args[0] == "-bench":
+    if args["bench"] is not None:
         pass
     
-    if args[0] == "-fft":
+    if args["fft"] is not None:
         pass
     
-    if args[0] == "-ll":
+    if args["ll"]:
         # Type check passed arguments
         inval = int(args[1])
         outval = ll.naive_lucas_lehmer(inval)
