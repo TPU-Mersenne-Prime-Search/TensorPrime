cba92c2fcdc11014488f159bd11ca8b207ce9554
jordenPDX <74211376+jordenPDX@users.noreply.github.com>
2022-01-20 16:53:08 -0800

    Added PRPTest code and updated main.py to utilize PRPTest

---
+++ PRPTest.py
@@ -0,0 +1,11 @@
+import time as time
+
+
+def probable_prime(power):
+    s = 3
+    for i in range(power):
+        s *= s
+        s = s % ((1 << power) - 1)
+    if s == 9:
+        return True
+    return False
--- main.py
+++ main.py
@@ -1,35 +1,42 @@
 import sys
 
 # Benchmarking
-#import . as bench
+# import . as bench
 # Lucas-Lehmer test
 import mersenne.lucas as ll
 # FFT
-#import . as fft
-
-
+# import . as fft
+from PRPTest import *
 
 
 def main():
     # Command line arguments
     args = sys.argv[1:]
-    
+
     # Argument option to load from file?
-    
+
     # Determine which function is wanted,
     # Run relevant function
-    
+
     if args[0] == "-bench":
         pass
-    
+
     if args[0] == "-fft":
         pass
-    
+
     if args[0] == "-ll":
         # Type check passed arguments
         inval = int(args[1])
         outval = ll.naive_lucas_lehmer(inval)
         print(outval)
-    
+
+    if args[0] == "-prp":
+        p = int(args[1])
+        start_time = time.time()
+        is_probable_prime = probable_prime(p)
+        end_time = time.time()
+        print("{} tested in {} sec: {}".format(p, end_time - start_time,
+                                               "probably prime!" if is_probable_prime else "composite"))
+
 
 main()
