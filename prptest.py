674d8a30810dc1b4cbba2120db8b144fb8285640
Maeve Hoffer <dhoffer@pdx.edu>
2022-02-13 23:13:55 -0800

    add status message every 100 iterations

--- prptest.py
+++ prptest.py
@@ -1,11 +1,16 @@
 import IBDWT as ibdwt
+import time
 
 def probable_prime(power):
+    start = time.time()
     s = 3
     for i in range(power):
         # s *= s
         # s = s % ((1 << power) - 1)
         s = ibdwt.squaremod_with_ibdwt(s)
+        if i % 100 == 0:
+            time_elapsed = time.time() - start
+            print("Time elapsed at iteration ", i, ": ", time_elapsed, ". S = ", s)
     if s == 9:
         return True
     return False
