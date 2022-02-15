6042415a4627f6585acd8df9643ce704f86328ab
Will Farris <9190155+WillFarris@users.noreply.github.com>
2022-02-15 11:24:05 -0800

    syntax error

--- prptest.py
+++ prptest.py
@@ -15,7 +15,7 @@ def probable_prime(power):
                 prev_d = d
                 d = (d * s) % n
             # Every L^2 iterations, check the current d value with and independently calculated d
-            if (i != 0 and i % L_2 == 0) or (i+ L > p):
+            if (i != 0 and i % L_2 == 0) or (i+ L > power):
                 check_value = (3 * (prev_d ** (2 ** L))) % n
                 if d != check_value:
                     i, s = rollback()
