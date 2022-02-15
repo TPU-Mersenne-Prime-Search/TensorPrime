c3b64fd42b78f7baa6148a61f521aa2436030b09
jordenPDX <74211376+jordenPDX@users.noreply.github.com>
2022-02-15 15:02:23 -0800

    Setup to just test ibdwt test

--- tests.py
+++ tests.py
@@ -11,31 +11,31 @@ import config
 # Demo class to showcase syntax for writing test cases
 
 
-class TestNumpyFunctionality(unittest.TestCase):
+# class TestNumpyFunctionality(unittest.TestCase):
 
-    # Demo test to check that Numpy's dot product works as expected
-    def test_dot_prod(self):
-        x = np.array([2, 3])
-        y = np.array([3, 2])
-        self.assertEqual(12, np.dot(x, y))
+#     # Demo test to check that Numpy's dot product works as expected
+#     def test_dot_prod(self):
+#         x = np.array([2, 3])
+#         y = np.array([3, 2])
+#         self.assertEqual(12, np.dot(x, y))
 
 
-class TestProbablePrimes(unittest.TestCase):
+# class TestProbablePrimes(unittest.TestCase):
 
-    # Test for probable prime test correctness
-    def test_prp_primes(self):
-        test_exponents = [7, 13, 17, 61, 89]
-        for i in range(len(test_exponents)):
-            config.initialize_constants(
-                test_exponents[i], 2**(floor(log2(test_exponents[i]))))
-            self.assertEqual(probable_prime(test_exponents[i]), True)
+#     # Test for probable prime test correctness
+#     def test_prp_primes(self):
+#         test_exponents = [7, 13, 17, 61, 89]
+#         for i in range(len(test_exponents)):
+#             config.initialize_constants(
+#                 test_exponents[i], 2**(floor(log2(test_exponents[i]))))
+#             self.assertEqual(probable_prime(test_exponents[i]), True)
 
-    def test_prp_composites(self):
-        test_exponents = [6, 12, 20, 100, 300]
-        for i in range(len(test_exponents)):
-            config.initialize_constants(
-                test_exponents[i], 2**(floor(log2(test_exponents[i]))))
-            self.assertEqual(probable_prime(test_exponents[i]), False)
+#     def test_prp_composites(self):
+#         test_exponents = [6, 12, 20, 100, 300]
+#         for i in range(len(test_exponents)):
+#             config.initialize_constants(
+#                 test_exponents[i], 2**(floor(log2(test_exponents[i]))))
+#             self.assertEqual(probable_prime(test_exponents[i]), False)
 
 
 class TestIBDWT(unittest.TestCase):
