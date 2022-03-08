1db903c7ff04672186d41c0629087ebf848aa1c2
Colton Jelsema <thecoltonjelsema@gmail.com>
2022-03-07 19:40:57 -0800

    Update
    Separate saving of the signal

--- saveload.py
+++ saveload.py
@@ -24,21 +24,25 @@ def save(exponent, siglen, signal, iteration):
         # Remove oldest saves if maximum is reached
         if i == maxsaves-1 and os.path.exists(pathed + str(maxsaves-1) + extension):
             os.remove(pathed + str(maxsaves-1) + extension)
+            os.remove(pathed + "signal" + str(maxsaves-1) + extension)
         
         # Increment saves
         for x in range(i):
             setval = i - x
             os.rename(pathed + str(setval - 1) + extension, 
                     pathed + str(setval) + extension)
+            os.rename(pathed + "signal" + str(setval - 1) + extension, 
+                    pathed +  "signal" + str(setval) + extension)
         break
     
     # Pack data
     # Exponent, Signal length are required to re-build
     # signal and iteration are required to resume within a prime check
-    packed = [exponent, siglen, iteration, signal]
+    packed = [exponent, siglen, iteration]
     
     # Save data
     np.save(pathed + "0", packed)
+    np.save(pathed + "signal0", signal)
     #.savez(pathed + "0", [config.exponent, iteration, config.signal_length], signal)
     
 
@@ -52,6 +56,7 @@ def load():
     
     # Load latest save file
     filedat = np.load(pathed + "0" + extension, allow_pickle = True)
+    filedat.append(np.load(pathed + "signal0" + extension, allow_pickle = True))
     
     # The signal length may be the cause of the problem
     # which would require it to be initialized from args
